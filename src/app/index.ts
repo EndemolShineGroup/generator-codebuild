2d337b821b4e428744b3751e05ddc33cc8c663ba
Hassan Khan <hassan.khan@endemolshine.com>
2018-11-04 17:44:54 +0000

    refactor(generator): remove unnecessary properties

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,24 +1,16 @@
-import path from 'path';
 import Generator from 'yeoman-generator';
 
 import prompts from './prompts';
 
-export default class NodeJsTypeScriptGenerator extends Generator {
-  public answers: Generator.Answers = {};
-
-  constructor(args: string | string[], opts: {}) {
-    super(args, opts);
-    this.sourceRoot(path.join(__dirname, 'templates'));
-  }
-
+export default class CodeBuildGenerator extends Generator {
   async initializing() {
     this.log('A few questions about your project...');
   }
 
   async prompting() {
-    this.answers = await this.prompt(prompts);
+    const answers = await this.prompt(prompts);
 
-    this.composeWith(require.resolve('../cloudformation'), this.answers);
-    this.composeWith(require.resolve('../project'), this.answers);
+    this.composeWith(require.resolve('../cloudformation'), answers);
+    this.composeWith(require.resolve('../project'), answers);
   }
 }
